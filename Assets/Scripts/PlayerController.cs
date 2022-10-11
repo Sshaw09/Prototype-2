da95c47a47f1d3137886bc6cb96ac3eff9a1b280
Sshaw09 <97680673+Sshaw09@users.noreply.github.com>
2022-10-11 12:45:57 -0400

    2.2 Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject projectilePrefab;
+
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
@@ -28,5 +30,11 @@ public class PlayerController : MonoBehaviour
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
