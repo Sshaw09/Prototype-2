6ba795d87c5e1d46bba42260be440f0b2bbade93
Sshaw09 <97680673+Sshaw09@users.noreply.github.com>
2022-10-12 17:41:30 -0400

    2.3 Complete

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float SpawnRangeX = 20.0f;
+    private float SpawnPosZ = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 SpawnPos = new Vector3(Random.Range(-SpawnRangeX, SpawnRangeX), 0, SpawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);  
+            Instantiate(animalPrefabs[animalIndex],SpawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
